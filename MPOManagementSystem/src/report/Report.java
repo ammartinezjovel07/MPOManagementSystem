8611349b88f7ab38e63a228c675d91d940d34bea
Raul Gomez <ralis14@yahoo.com>
2016-05-09 11:05:14 -0600

    db

--- MPOManagementSystem/src/report/Report.java
+++ MPOManagementSystem/src/report/Report.java
@@ -1,5 +1,20 @@
 package report;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import database.DatabaseHandler;
+import database.Project;
+
 public class Report {
 
+	
+	public ArrayList<Project> generateTIPReport(int year){
+		return new DatabaseHandler().retrieveTIP(year);
+	}
+	
+	public ArrayList<Project> generateMTPReport(int year){
+		return new DatabaseHandler().retrieveMTP(year);
+	}
+	
 }
