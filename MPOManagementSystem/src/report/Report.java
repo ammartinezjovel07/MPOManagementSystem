dc345243a119bfa9ef7c205c5971b1f43e669583
ammartinezjovel07 <ammartinezjovel@gmail.com>
2016-05-09 10:29:07 -0600

    added needed fields for report. Missing method calls to view reports and generate reports

--- MPOManagementSystem/src/report/Report.java
+++ MPOManagementSystem/src/report/Report.java
@@ -1,5 +1,29 @@
 package report;
 
-public class Report {
-
+public class Report
+{	
+	boolean capacityProject;
+	String city;
+	String county;
+	String dotDistrict;
+	int federalFiscalYear;
+	String federalFunctionalClassification;
+	String fundProjectType;
+	String highwayRoadwayName;
+	String limitFrom;
+	String limitTo;
+	int networkYear;
+	int numberExistingLanes;
+	long numberMiles;
+	int numberProjectedLines;
+	String projectDescription;
+	String projectID;
+	String tipProjectName;
+	String projectPhase;
+	String projectType;
+	String remarks;
+	String sponsorAgency;
+	String transitInformation;
+	long projectCost;
+	long projectFunding;
 }
