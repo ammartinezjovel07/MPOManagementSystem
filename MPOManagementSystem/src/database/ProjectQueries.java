9329bd13b53732c98cebf9348f90f6d70b12a44b
Raul Gomez <ralis14@yahoo.com>
2016-05-10 21:35:07 -0600

    project queries

--- MPOManagementSystem/src/database/ProjectQueries.java
+++ MPOManagementSystem/src/database/ProjectQueries.java
@@ -1,11 +1,220 @@
 package database;
 
+import java.sql.PreparedStatement;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class ProjectQueries {
+	String projectId = "Select * from projects where projectId = ";
+	
+/*
+ * 
+ * Every table needs a link to pid
+ * @param projectInfo
+ * @return
+ */
+	
+	
+	public String createCostQuery(LinkedHashMap elements, int p_id)
+	{
+		String query = "Insert Into cost(p_id, constructionCost, constCost, contingencyCost, ftaTransferCost, "
+				+ "nonConstructionCost, changeOrderCost, preEngCost, rightofWayCost)"
+				+ "Values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		return query;
+//		Set<Object> items= elements.keySet();
+//		int i = 0;
+//	     for(Object k: items)
+//	     {
+//	    	 
+//	        System.out.println(query += elements.get(k));
+//	     }
+	}
+	public String createFundingQuery()
+	{
+		String query = "Insert Into funding(p_id, federalShare, fundingCategory, localContribution, localShare, "
+				+ "regionalShare, stateShare)"
+				+ "Values(?, ?, ?, ?, ?, ?, ?)";
+		return query;
+	}
+	public String createOrganizationQuery()
+	{
+		String query = "Insert Into organization(p_id, contactFirstName, contactMiddleInitial, contactLastName, contactPositionTitle, "
+				+ "contactDeptNumber, contactEmail, organizationName, workContactNumber)"
+				+ "Values(?, ?, ?, ?, ?, ?, ?)";
+		return query;
+	}
+	public String createFundedProjectQuery()
+	{
+		String query = "Insert Into fundedProjects(p_id, ammendmentDate, cnNumber, csjNumber, tipName, "
+				+ "tipYear)"
+				+ "Values(?, ?, ?, ?, ?, ?)";
+		return query;
+	}
+	public String createFundedTransitQuery()
+	{
+		String query = "Insert Into fundedTransitProjects(p_id, tdcAwardAmmount, tdcAwardDate "
+				+ ")"
+				+ "Values(?, ?, ?)";
+		return query;
+	}
+	
+	public String createProposedProjectQuery()
+	{
+		String query = "Insert Into proposedProjects"
+				+ "(p_id, approvedProjectId, submissionDeadline "
+				+ "safetyImproved, trafficCalming, comprehensivePlan,"
+				+ "transitServiceProgram, feasibilityStidies, gradedSeparatedCrossing,"
+				+ "impavedRoads, improvedAirQuality, localSupportCommunity,"
+				+ "localSupportGov, localSupportSchool, connectResAreas,"
+				+ "connectIndAreas, connectCommAreas, connectBikePath, "
+				+ "connectRuralAreas, connectPedPath, connectTransit, "
+				+ "connectRecAct, sustainLandUse, sustainEconomy, "
+				+ "sustainRemoveBarriers, fulfillMap21Goal, fulfillMap21Infra, "
+				+ "fulfillMap21Congress, fulfillMap21SysRel, fulfillMap21Vitality, "
+				+ "fulfillMap21Sustain, satisfyCMPManage, satisfyCMPTraffic, "
+				+ "satisfyCMPPubTransp, satisfyCMPRoadCap, satisfyNonCMP, "
+				+ "sponsorInvesCost, commitToFederalFunding, phaseInvest, "
+				+ "submissionType, workflowStatus)"
+				+ "Values("
+				+ "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+				+ "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+				+ "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+				+ "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+				+ "?)";
+		return query;
+	}
+	public String createProposedTransitQuery()
+	{
+		String query = "Insert Into proposedTransitProjects"
+				+ "(p_id, apportionmentYear, section5309ID "
+				+ "tdcAmmountRequest, transitProjectType)"
+				+ "Values(?, ?, ?, ?, ?)";
+		return query;
+	}
+	//finish
+	public String createProjectQuery()
+	{
+		String query = "Insert Into projects"
+				+ "(p_id, projectTitle, projectId, projectDescription, projectPhases, "
+				+ "projectType, termini, network, totalProjectCost, sponsor, yoe,"
+				+ "airQualityAnalysisCO, airQualityAnalysisNOX, airQualityAnalysisVOC, area, capacityProject,"
+				+ "city, county, dotDistrict, federalFiscalyear, federalFunctionalClassification, fundProjectType,"
+				+ "highwayRoadwayName, limitFrom, limitTo, networkYear, numberMiles, numberExistingLanes, "
+				+ "numberProjectedLanes, remarks, scopeOfProduct, stateSystemRoad, transitInformation)"
+				+ "Values("
+				+ "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+				+ "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+				+ "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+				+ "?, ?, ? )";
+		return query;
+	}
+	
+	
+	
+	//retrieve queries
+	public String retrieveCostQuery(int p_id)
+	{
+		String query =  "Select * from cost where p_id = " + p_id;
+		return query;
+	}
+	public String retrieveFundingQuery(int p_id)
+	{
+		String query =  "Select * from funding where p_id = " + p_id;
+		return query;
+	}
+	public String retrieveOrganizationQuery(int p_id)
+	{
+		String query =  "Select * from orgnization where p_id = " + p_id;
+		return query;
+	}
+	public String retrieveFundedProjectQuery(int p_id)
+	{
+		String query =  "Select * from fundedProjects where p_id = " + p_id;
+		return query;
+	}
+	public String retrieveFundedTransitQuery(int p_id)
+	{
+		String query =  "Select * from fundedTransit where p_id = " + p_id;
+		return query;
+	}
+	public String retrieveProposedProjectQuery(int p_id)
+	{
+		String query =  "Select * from proposedProjects where p_id = " + p_id;
+		return query;
+	}
+	public String retrieveProposedTransitQuery(int p_id)
+	{
+		String query =  "Select * from proposedTransitProjects where p_id = " + p_id;
+		return query;
+	}
+	public String retrieveProject(int p_id)
+	{
+		String query =  "Select * from projects where p_id = " + p_id;
+		return query;
+	}
 
+	
+	public String updateCostQuery(){return null;}
+	public String updateFundingQuery(){return null;}
+	public String updateOrganizationQuery(){return null;}
+	public String updateFundedProjectQuery(){return null;}
+	public String updateTransitQuery(){return null;}
+	public String updateProposedProjectQuery(){return null;}
+	public String updateProposedTransitQuery(){return null;}
+	
+	
+	// Delete queries
+	public String deleteCostQuery(int p_id)
+	{
+		String query =  "delete from cost where p_id = " + p_id;
+		return query;
+	}
+	public String deleteFundingQuery(int p_id)
+	{
+		String query =  "delete from funding where p_id = " + p_id;
+		return query;
+	}
+	public String deleteOrganizationQuery(int p_id)
+	{
+		String query =  "delete from organization where p_id = " + p_id;
+		return query;
+	}
+	public String deleteFundedProjectQuery(int p_id)
+	{
+		String query =  "delete from fundedProjects where p_id = " + p_id;
+		return query;
+	}
+	public String deleteFundedTransitQuery(int p_id)
+	{
+		String query =  "delete from fundedTransit where p_id = " + p_id;
+		return query;
+	}
+	public String deleteProposedProjectQuery(int p_id)
+	{
+		String query =  "delete from proposedProjects where p_id = " + p_id;
+		return query;
+	}
+	public String deleteProposedTransitQuery(int p_id)
+	{
+		String query =  "delete from proposedTransitProjects where p_id = " + p_id;
+		return query;
+	}
+	public String deleteProject(int p_id)
+	{
+		String query =  "delete from projects where p_id = " + p_id;
+		return query;
+	}
+	
+	
+	
 	public  String createProjectQueries(Map<String, String> projectInfo){return null;}
-	public String retriveProjectQueries(int id){return null;}
+	public String retriveProjectQueries(String projectId)
+	{
+		projectId += projectId;
+		return projectId;
+	}
 	public String updateProjectQueries(Map<String, String> projectInfo, int id){return null;}
-	public String deleteProjectQueries(int id){return null;}
+
 }
+
--- MPOManagementSystem/src/database/TestMain.java
+++ MPOManagementSystem/src/database/TestMain.java
@@ -1,4 +1,8 @@
 package database;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
 import user.*;
 
 
@@ -8,15 +12,27 @@ public class TestMain {
 	public static void main(String args[])
 	{
 	
-		Administrator admin = new Administrator();
-		admin.createAccount("luis", 'c', "luis", "luis", "luis", "luis", "luis", "luis", "luis", "luis", "luis", "luis", true);
-		admin.deleteAccount("me");
-		admin.updateAccount("lues", "email", "here");
+//		Administrator admin = new Administrator();
+//		admin.createAccount("luis", 'c', "luis", "luis", "luis", "luis", "luis", "luis", "luis", "luis", "luis", "luis", true);
+//		admin.deleteAccount("me");
+//		admin.updateAccount("lues", "email", "here");
+//		
+//		MPOLeadUser mpoLead = new MPOLeadUser();
+//		mpoLead.approveAccount("luis");
+//		
+//		RegularUser ruser = new RegularUser();
+//		ruser.verifyCredentials("luis", "not");
 		
-		MPOLeadUser mpoLead = new MPOLeadUser();
-		mpoLead.approveAccount("luis");
+        LinkedHashMap projectElements = new LinkedHashMap <String, Object>();
+        
+		projectElements.put("projectTitle", "P_Title");
+		projectElements.put("projectID", 8847);
+		projectElements.put("projectDescription", "Description");
+		projectElements.put("projectPhases", "phase1");
+		projectElements.put("projectType", 5);
 		
-		RegularUser ruser = new RegularUser();
-		ruser.verifyCredentials("luis", "not");
+     Set<Object> items= projectElements.keySet();
+     System.out.println(items);
+    		 
 	}
 }
